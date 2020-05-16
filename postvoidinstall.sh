65f2682d762369ed286d6ee89d1eb7cf093513d1
sarcasticdream <54422164+sarcasticdream@users.noreply.github.com>
2020-05-16 01:20:12 -0500

    Update postvoidinstall.sh

--- postvoidinstall.sh
+++ postvoidinstall.sh
@@ -1,10 +1,10 @@
 cd ~/
 echo"""Basically this will setup xorg and dwm with some other useful things you might need for your journey"""
 echo"""Updating System"""
-sudo xbps-install -Su
-sudo xbps-install -Su
+sudo xbps-install -Syu
+sudo xbps-install -Syu
 echo"""Installing Packages"""
-sudo xbps-install -S xorg git curl wget firefox base-devel libX11-devel libXft-devel libXinerama-devel bash-completion vim nitrogen
+sudo xbps-install -Sy xorg git curl wget firefox base-devel libX11-devel libXft-devel libXinerama-devel bash-completion vim nitrogen
 echo"""Cloning the repos"""
 git clone https://git.suckless.org/dwm
 git clone https://github.com/LukeSmithxyz/st
