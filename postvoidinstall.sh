29abfa99ca758a13a7d23e7d3dbc291fcea7648c
sarcasticdream <54422164+sarcasticdream@users.noreply.github.com>
2020-05-16 11:06:27 -0500

    Update postvoidinstall.sh

--- postvoidinstall.sh
+++ postvoidinstall.sh
@@ -12,12 +12,13 @@ git clone https://git.suckless.org/dmenu
 git clone https://sarcasticdream/voidshiz
 echo"""Compiling the shiz"""
 cd ~/dwm
-sudo sed -i '/X11INC = usr/X11R6/include/X11INC = usr/include/X11/g' config.mk
-sudo sed -i '/X11LIB = usr/X11R6/lib/X11LIB = usr/lib/X11/g' config.mk
+sudo sed -i 's/X11INC = usr/X11R6/include/X11INC = usr/include/X11/g' config.mk
+sudo sed -i 's/X11LIB = usr/X11R6/lib/X11LIB = usr/lib/X11/g' config.mk
 cd ~/st
 sudo make clean install
 cd ~/dmenu
 sudo make clean install
+cd ~/
 touch .xinitrc
 echo """
 In your xinitrc please change the resolution to one your are comfortable with
